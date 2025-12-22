2cd4ac498000f0a3b4a188a5551e476be3cd3df6
mate_gvetadze <136162687+mategvetadze@users.noreply.github.com>
2025-12-22 15:12:41 +0400

    v0.3

--- giftart/backend/urls.py
+++ giftart/backend/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('aboutus/', views.aboutus, name='aboutus'),
+    path('contact/', views.contact, name='contact'),
+    path('shop/', views.shop, name='shop'),
+    path('order/', views.order, name='order'),
+    path('order/photo/', views.order_photo, name='order_photo'),
+    path('order/video/', views.order_video, name='order_video'),
+    path('admin/login/', views.admin_login, name='admin_login'),
+    path('admin/dashboard/', views.admin_dashboard, name='admin_dashboard'),
 ]
--- giftart/backend/views.py
+++ giftart/backend/views.py
@@ -1,7 +1,33 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, "home.html")
+
+def aboutus(request):
+    return render(request, "aboutus.html")
+
+def contact(request):
+    return render(request, "contact.html")
+
+
+def shop(request):
+    return render(request, "shop.html")
+
+def order(request):
+    return render(request, "order.html")
+
+def order_photo(request):
+    return render(request, "order_photo.html")
+
+
+def order_video(request):
+    return render(request, "order_video.html")
+
+def admin_login(request):
+    return HttpResponse("Hello World")
+
+def admin_dashboard(request):
+    return HttpResponse("Hello World")
